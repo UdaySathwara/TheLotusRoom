50d330ab7210898fe18be76f6fed1d1108ceec4f
UdaySathwara <sathwarauday24@gmail.com>
2025-04-01 17:31:08 +0530

    Full and Final

--- src/Components/Atoms/Navbar.jsx
+++ src/Components/Atoms/Navbar.jsx
@@ -26,18 +26,18 @@ const Navbar = ({ user, handleLogout }) => {
             onClick={toggleMenu}
           >
             <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 rotate-180"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d={
-                  isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"
+                  isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4  6h16M4 6h16M4 12h16M4 12h16M4 18h16M4 18h16"
                 }
               ></path>
             </svg>
