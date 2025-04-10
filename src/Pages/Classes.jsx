536b45f24b2be8eb10c8a81338c3dcc681d93fd8
UdaySathwara <sathwarauday24@gmail.com>
2025-04-10 11:59:18 +0530

    Done

--- src/Pages/Classes.jsx
+++ src/Pages/Classes.jsx
@@ -7,13 +7,13 @@ function Classes() {
     <div>
 
       <section
-        className="flex flex-col items-center justify-center text-center h-screen w-full bg-cover bg-center bg-no-repeat relative p-6"
+        className="flex flex-col items-center justify-center text-center h-screen w-full bg-cover bg-bottom bg-no-repeat relative p-6"
         style={{
           backgroundImage:
-            "url('https://img.freepik.com/free-photo/women-their-yoga-session_23-2149222428.jpg?t=st=1743500601~exp=1743504201~hmac=bc441cb48e5927fd524a48a027db527424d3c83fd475af66a0040ba251ac61e1&w=1380')",
+            "url('https://img.freepik.com/free-photo/yoga-group-classes-inside-gym_1303-14788.jpg?semt=ais_country_boost&w=740')",
         }}
       >
-        <div className="absolute inset-0 bg-black/50"></div>
+        <div className="absolute inset-0 bg-black/55"></div>
 
         {/* Content */}
         <div className="relative z-10 max-w-3xl px-4">
